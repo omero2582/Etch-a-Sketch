abc14c08e1a2be79b115aa076eda2f51fd533fa6
omero2582 <iiomero2582ii@gmail.com>
2022-04-05 00:02:38 -0400

    finish basic drag & hold w/ problem
    sometimes the mouseup event is not fired. It looks like
    sometimes mousedown is being detected as a 'drag' instead.
    need to figure something out. sleeepy

--- script.js
+++ script.js
@@ -1,31 +1,58 @@
 container = document.querySelector(".container");
-container.style.width = '800px';
-container.style.height = '800px';
+let width = container.offsetHeight;
+let height = container.offsetWidth;
 
 function createSquares(num){
-    for (let i = 0; i < num*num ;i++){
+    for (let i = 0; i < (num*num); i++){
         square = document.createElement('div');
-        square.style.cssText = `width: 100px; height: 100px; background-color: white;`
+        square.style.cssText = `width: ${width/num}px; height: ${height/num}px; background-color: white;`
         square.style.cssText += 'border: 2px solid black;';
         square.style.cssText += 'box-sizing: border-box;';
-        square.addEventListener('click' , changeBackground);
+        square.setAttribute('draggable', 'false');
+        square.addEventListener('mousedown' , changeBackground);
+        square.addEventListener('mouseenter' , changeBackground);
         square.classList.add('square');
+        //
         container.appendChild(square);
     }
     
 }
 
-createSquares(8);
+let mouseDown = false;
+document.addEventListener( 'mousedown', onMouseDown, false ); 
+document.addEventListener( 'mouseup', onMouseUp, false );
+
+function onMouseDown(e) {
+    mouseDown = true;
+    console.log('1');
+    e.stopPropagation();
+}
+function onMouseUp (e) {
+    mouseDown = false;
+    console.log('2');
+}
+createSquares(15);
 
 function changeBackground (e){
-    console.log(this);
+    if(!mouseDown){
+        console.log('hi');
+        return;
+    }
     this.style.backgroundColor = this.style.backgroundColor == 'black' ? 'white' : 'black';
     e.stopPropagation();
+    //  ^^ stopPropagation was preventing the click & hold loging from working
 }
 
 const button = document.querySelector('.clear');
 button.addEventListener('click', clear)
 
+function clear(e){
+    const allSquares = document.querySelectorAll('.square');
+    console.log(allSquares);
+    allSquares.forEach( square => square.style.backgroundColor= 'white');
+}
+
+//prev random functions
 function pickColor(){
     const colors = ['blue', 'black', 'red', 'green'];
     return colors[random(4)];
@@ -35,8 +62,3 @@ function random(num){
     return Math.floor(Math.random() * num);
 }
 
-function clear(e){
-    const allSquares = document.querySelectorAll('.square');
-    console.log(allSquares);
-    allSquares.forEach( square => square.style.backgroundColor= 'white');
-}
