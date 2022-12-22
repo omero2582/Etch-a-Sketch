0b84adbd4db9d2ee42a14ac412c884b50f6c7780
omero2582 <iiomero2582ii@gmail.com>
2022-12-22 11:23:57 -0500

    Fixed mouse click up detection to no longer paint
    Using 'mouseover' instead of 'mouseenter' as a click listener.
    Also inside onMouseDown(), i am using preventDefault instead of
    stopPropagation(). Also in changeBackground we changed the if check to
    if(e.type == 'mouseover' && !mouseDown) --- instead of if(!mouseDown)
    
    No idea why, because I am just comming back to simply commit the changes
    after a long time. But I know it fixes click up detection, which was
    previousely not working well, and would instead keep painting black
    sometimes

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 container = document.querySelector(".container");
+//container.setAttribute('draggable', false);
+//document.querySelector('body').setAttribute('draggable', false);
 let width = container.offsetHeight;
 let height = container.offsetWidth;
 
@@ -8,9 +10,9 @@ function createSquares(num){
         square.style.cssText = `width: ${width/num}px; height: ${height/num}px; background-color: white;`
         square.style.cssText += 'border: 2px solid black;';
         square.style.cssText += 'box-sizing: border-box;';
-        square.setAttribute('draggable', 'false');
+        square.setAttribute('draggable', false);
         square.addEventListener('mousedown' , changeBackground);
-        square.addEventListener('mouseenter' , changeBackground);
+        square.addEventListener('mouseover' , changeBackground);
         square.classList.add('square');
         //
         container.appendChild(square);
@@ -25,7 +27,9 @@ document.addEventListener( 'mouseup', onMouseUp, false );
 function onMouseDown(e) {
     mouseDown = true;
     console.log('1');
-    e.stopPropagation();
+    //e.stopPropagation();
+    e.preventDefault();  // <<<<<<<<<<<<< THIS SOLVED THE MOUSEUP EVENT not firing sometimes
+    //no idea why it works tho and stopPropagation doesnt
 }
 function onMouseUp (e) {
     mouseDown = false;
@@ -34,12 +38,13 @@ function onMouseUp (e) {
 createSquares(15);
 
 function changeBackground (e){
-    if(!mouseDown){
+    if(e.type == 'mouseover' && !mouseDown){
         console.log('hi');
         return;
     }
-    this.style.backgroundColor = this.style.backgroundColor == 'black' ? 'white' : 'black';
-    e.stopPropagation();
+    this.style.backgroundColor = this.style.backgroundColor == 'black' ? 'black' : 'black';
+    // ^^^^ took away 'if black -> turn white', because harder to write letters when u go over drawn lines
+    //- was " =='black' ? 'white' : 'black'; "
     //  ^^ stopPropagation was preventing the click & hold loging from working
 }
 
